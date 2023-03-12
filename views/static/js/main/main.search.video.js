ffd0531aea0641756f0bbdaa1c56085160bdd585
Neo <106986582+Poodly@users.noreply.github.com>
2023-03-13 06:34:49 +0900

    redis활용 검색결과 캐싱, 비디오 정보 캐싱, 검색로직 3계층 분류, 폴더구조 변경, 로딩로직 수정

--- views/static/js/main/main.search.video.js
+++ views/static/js/main/main.search.video.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    searchVideo();
+document.addEventListener('DOMContentLoaded', async function() {
+    await searchVideo();
 });
 
 function searchVideo() {
@@ -16,38 +16,100 @@ function searchVideo() {
         const response = await axios.post(url, { query });
         const videoIds = await response.data
 
-        for (let i = 0; i < videoIds.length; i++) {
-            const videoId    = videoIds[i];
-            const videoInfo  = await getVideoInfo(videoId);
+        // 비디오 정보를 병렬로 가져오기 => ㄷㄷ 뭐지이건.. 공부해 보기
+        const videoInfoPromises = videoIds.map(videoId => getVideoInfo(videoId));
+        const videoInfos = await Promise.all(videoInfoPromises);
 
-            let thumbnailUrl = videoInfo.thumbnailUrl;
-            let title        = videoInfo.title;
-            let description  = videoInfo.description;
-            let tags         = videoInfo.tags;
-            let viewCount    = videoInfo.viewCount;
-            console.log("viewCount ---", viewCount);
+        // 검색 결과에 따라 HTML 코드 생성
+        const tempHtmlArray = [];
+        for (let i = 0; i < videoIds.length; i++) {
+            const videoId = videoIds[i];
+            const videoInfo = videoInfos[i];
+
+            const {
+                thumbnailUrl,
+                title,
+                description,
+                tags,
+                viewCount
+            } = videoInfo;
 
             const descriptionKeywords = isKeywordsIncluded(title , searchKeyword)
             const titleKeywords = isKeywordsIncluded(description , searchKeyword)
             const tagesKeywords = isKeywordsIncludedArr(tags, searchKeyword);
 
             if (tagesKeywords || titleKeywords || descriptionKeywords) {
-                console.log("Keywords is true ^^")
                 const tempHtml = `<div class="col-xl-4 col-lg-4 col-md-6">
                                     <div class="card" style="width: 18rem;">
-                                        <a href="#" onclick="modalPlayVideo('${videoId}','${title.replace(/'/g, '').replace(/"/g, '')}','${viewCount}')" data-toggle="modal" data-target="#myModal">
-                                            <img src="${thumbnailUrl}" class="card-img-top" alt="${title}">
-                                            <div class="card-body">
-                                                <p class="card-title">${title}</p>
-                                            </div>
-                                        </a>
+                                    <a href="#" onclick="modalPlayVideo('${videoId}','${title.replace(/'/g, '').replace(/"/g, '')}','${viewCount}')" data-toggle="modal" data-target="#myModal">
+                                        <img src="${thumbnailUrl}" class="card-img-top" alt="${title}">
+                                        <div class="card-body">
+                                        <p class="card-title">${title}</p>
+                                        </div>
+                                    </a>
                                     </div>
                                 </div>`;
-                    searchContainer.insertAdjacentHTML('beforeend', tempHtml);
-                    }
+                tempHtmlArray.push(tempHtml);
                 }
-            } catch (error) {
+            }
+            searchContainer.insertAdjacentHTML('beforeend', tempHtmlArray.join(''));
+        } catch (error) {
             console.log('Error:', error);
         }
     });
-}
+};
+
+
+// document.addEventListener('DOMContentLoaded', async function() {
+//     await searchVideo();
+// });
+
+// function searchVideo() {
+//     const searchButton = document.getElementById('search-button');
+//     searchButton.addEventListener('click', async () => {
+//         const url = '/video/search'
+//         const query  = document.getElementById('search_box').value;
+
+//         // 이전 검색 결과 지우기
+//         const searchContainer = document.getElementById('search-container');
+//         searchContainer.innerHTML = '';
+
+//     try {
+//         const response = await axios.post(url, { query });
+//         const videoIds = await response.data
+
+//         for (let i = 0; i < videoIds.length; i++) {
+//             const videoId    = videoIds[i];
+//             const videoInfo  = await getVideoInfo(videoId);
+
+//             let thumbnailUrl = videoInfo.thumbnailUrl;
+//             let title        = videoInfo.title;
+//             let description  = videoInfo.description;
+//             let tags         = videoInfo.tags;
+//             let viewCount    = videoInfo.viewCount;
+//             console.log("viewCount ---", viewCount);
+
+//             const descriptionKeywords = isKeywordsIncluded(title , searchKeyword)
+//             const titleKeywords = isKeywordsIncluded(description , searchKeyword)
+//             const tagesKeywords = isKeywordsIncludedArr(tags, searchKeyword);
+
+//             if (tagesKeywords || titleKeywords || descriptionKeywords) {
+//                 console.log("Keywords is true ^^")
+//                 const tempHtml = `<div class="col-xl-4 col-lg-4 col-md-6">
+//                                     <div class="card" style="width: 18rem;">
+//                                         <a href="#" onclick="modalPlayVideo('${videoId}','${title.replace(/'/g, '').replace(/"/g, '')}','${viewCount}')" data-toggle="modal" data-target="#myModal">
+//                                             <img src="${thumbnailUrl}" class="card-img-top" alt="${title}">
+//                                             <div class="card-body">
+//                                                 <p class="card-title">${title}</p>
+//                                             </div>
+//                                         </a>
+//                                     </div>
+//                                 </div>`;
+//                     searchContainer.insertAdjacentHTML('beforeend', tempHtml);
+//                     }
+//                 }
+//             } catch (error) {
+//             console.log('Error:', error);
+//         }
+//     });
+// }
--- views/static/js/main/main.start.video.js
+++ views/static/js/main/main.start.video.js
@@ -1,35 +1,41 @@
-$(document).ready(function () {
-    mainStartVideo();
+document.addEventListener('DOMContentLoaded', async function() {
+    await mainStartVideo();
 });
 
 async function mainStartVideo() {
     const url = '/video/startSearch';
-    
+
     // 이전 검색 결과 지우기
     const searchContainer = document.getElementById('search-container');
     searchContainer.innerHTML = '';
 
     try {
         const response = await axios.get(url);
-        const videoIds = await response.data
+        const videoIds = await response.data;
 
+        // 비디오 정보를 병렬로 가져오기 => ㄷㄷ 뭐지이건.. 공부해 보기
+        const videoInfoPromises = videoIds.map(videoId => getVideoInfo(videoId));
+        const videoInfos = await Promise.all(videoInfoPromises);
+
+        // 검색 결과에 따라 HTML 코드 생성
+        const tempHtmlArray = [];
         for (let i = 0; i < videoIds.length; i++) {
-            const videoId    = videoIds[i];
-            const videoInfo  = await getVideoInfo(videoId);
+            const videoId = videoIds[i];
+            const videoInfo = videoInfos[i];
 
-            let thumbnailUrl = videoInfo.thumbnailUrl;
-            let title        = videoInfo.title;
-            let description  = videoInfo.description;
-            let tags         = videoInfo.tags;
-            let viewCount    = videoInfo.viewCount;
-            console.log("viewCount ---", viewCount);
+            const {
+                thumbnailUrl,
+                title,
+                description,
+                tags,
+                viewCount
+            } = videoInfo;
 
             const descriptionKeywords = isKeywordsIncluded(title , searchKeyword)
             const titleKeywords = isKeywordsIncluded(description , searchKeyword)
             const tagesKeywords = isKeywordsIncludedArr(tags, searchKeyword);
 
-            if (tagesKeywords || titleKeywords || descriptionKeywords) {
-                console.log("Keywords is true ^^")
+            if (descriptionKeywords || titleKeywords || tagesKeywords) {
                 const tempHtml = `<div class="col-xl-4 col-lg-4 col-md-6">
                                     <div class="card" style="width: 18rem;">
                                     <a href="#" onclick="modalPlayVideo('${videoId}','${title.replace(/'/g, '').replace(/"/g, '')}','${viewCount}')" data-toggle="modal" data-target="#myModal">
@@ -40,11 +46,66 @@ async function mainStartVideo() {
                                     </a>
                                     </div>
                                 </div>`;
-                searchContainer.insertAdjacentHTML('beforeend', tempHtml);
-                }
+                tempHtmlArray.push(tempHtml);
             }
-        } catch (error) {
+        }
+        searchContainer.insertAdjacentHTML('beforeend', tempHtmlArray.join(''));
+    } catch (error) {
         console.log('Error:', error);
     }
 }
+
+
+
+// 이전 코드.. 시간복잡도 개선문의해서 더 나아진듯 ㄷㄷ
+
+// document.addEventListener('DOMContentLoaded', async function() {
+//     await mainStartVideo();
+// });
+
+// async function mainStartVideo() {
+//     const url = '/video/startSearch';
+    
+//     // 이전 검색 결과 지우기
+//     const searchContainer = document.getElementById('search-container');
+//     searchContainer.innerHTML = '';
+
+//     try {
+//         const response = await axios.get(url);
+//         const videoIds = await response.data
+
+//         for (let i = 0; i < videoIds.length; i++) {
+//             const videoId    = videoIds[i];
+//             const videoInfo  = await getVideoInfo(videoId);
+
+//             let thumbnailUrl = videoInfo.thumbnailUrl;
+//             let title        = videoInfo.title;
+//             let description  = videoInfo.description;
+//             let tags         = videoInfo.tags;
+//             let viewCount    = videoInfo.viewCount;
+//             console.log("viewCount ---", viewCount);
+
+//             const descriptionKeywords = isKeywordsIncluded(title , searchKeyword)
+//             const titleKeywords = isKeywordsIncluded(description , searchKeyword)
+//             const tagesKeywords = isKeywordsIncludedArr(tags, searchKeyword);
+
+//             if (tagesKeywords || titleKeywords || descriptionKeywords) {
+//                 console.log("Keywords is true ^^")
+//                 const tempHtml = `<div class="col-xl-4 col-lg-4 col-md-6">
+//                                     <div class="card" style="width: 18rem;">
+//                                     <a href="#" onclick="modalPlayVideo('${videoId}','${title.replace(/'/g, '').replace(/"/g, '')}','${viewCount}')" data-toggle="modal" data-target="#myModal">
+//                                         <img src="${thumbnailUrl}" class="card-img-top" alt="${title}">
+//                                         <div class="card-body">
+//                                         <p class="card-title">${title}</p>
+//                                         </div>
+//                                     </a>
+//                                     </div>
+//                                 </div>`;
+//                 searchContainer.insertAdjacentHTML('beforeend', tempHtml);
+//                 }
+//             }
+//         } catch (error) {
+//         console.log('Error:', error);
+//     }
+// }
   
