c883f8fcf20b6cdf6f43a17f7aadcdb53c28d67a
Debian <bitnami@ip-172-26-8-2.ap-northeast-2.compute.internal>
2023-03-31 18:56:12 +0000

    main

--- views/static/js/idolRank/idol.rank.loader.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', async function() {
-    $('#spinner-box').show()
-    await makeRankCard();
-    $('#spinner-box').hide()
-    window.scrollTo(0, 175);
-});
--- views/static/js/user.profile/user.loader.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', async function() {
-    $("#spinner-box").show()
-    await searchLikeVideos();
-    $("#spinner-box").hide()
-    window.scrollTo(0, 185);
-});
